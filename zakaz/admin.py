2e1352cf24eb7420ea7a2f50ed8fb07591d43023
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-03-21 23:07:33 +0200

    Update admin.py

--- zakaz/admin.py
+++ zakaz/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import City, Region, Area, Order, TypeWork
 
-# Register your models here.
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TypeWork)
+class TypeWorkAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
