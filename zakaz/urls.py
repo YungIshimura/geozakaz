903d69c75f8298bf88c0d23fd3e031822d6cd1c1
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-03-21 23:07:37 +0200

    Fix naming

--- zakaz/urls.py
+++ zakaz/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from zakaz.views import application_view, application_pages_view
+from zakaz.views import view_application, view_application_pages
 
 app_name = 'zakaz'
 
 urlpatterns = [
-    path('application/', application_view, name='application'),
-    path('application_pages/', application_pages_view, name='application_pages')
+    path('application/', view_application, name='application'),
+    path('application_pages/', view_application_pages, name='application_pages')
 ]
--- zakaz/views.py
+++ zakaz/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect, render
+from django.urls import reverse
+from .forms import OrderForm, OrderFileForm
 
 
-def application_view(request):
-    return render(request, 'application.html')
+def view_application(request):
+    context = {}
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST)
+        order_files_form = OrderFileForm(request.POST, request.FILES)
+        if order_form.is_valid() and order_files_form.is_valid():
+            order=order_form.save()
+            order_files = order_files_form.save(commit=False)
+            order_files.order = order
+            order_files.save()
+
+            return HttpResponseRedirect(reverse('zakaz:application_pages'))
+        else:
+            print(order_form.errors)
+            print(order_files_form.errors)
+    else:
+        order_form = OrderForm()
+        order_files_form = OrderFileForm()
+
+    context['order_form'] = order_form
+    context['order_files_form'] = order_files_form
+
+    return render(request, 'application.html', context=context)
 
 
-def application_pages_view(request):
+def view_application_pages(request):
     return render(request, 'application_pages.html')
