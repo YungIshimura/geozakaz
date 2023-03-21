e5c0171b13a48f8052c4800a89ceca8e808428ad
ghost <kirill.han011@gmail.com>
2023-03-21 21:17:24 +0300

    Добавлена страница просмотра заявок. Выполнена частичная правка тегов и стилей

--- zakaz/views.py
+++ zakaz/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def application_view(request):
     return render(request, 'application.html')
+
+
+def application_pages_view(request):
+    return render(request, 'application_pages.html')
