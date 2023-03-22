94eb328006bb6f57ac2066c8870ac0277e835df5
axexeyvershinin <og2001@yandex.ru>
2023-03-22 14:59:50 +0300

    Added agreement, refactoring code

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from users.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'phone', 'first_name', 'last_name', 'patronymic', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'patronymic', 'email', 'phone')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -6,4 +6,5 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class User(AbstractUser):
     patronymic = models.CharField('Отчество', max_length=30)
-    phone = PhoneNumberField()
+    phone = PhoneNumberField('Телефон')
+    agreement = models.BooleanField('Принял условия соглашения', default=False)
