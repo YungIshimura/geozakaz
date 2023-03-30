ef55992291d5513f79ab17e5485ee10687757125
ghost <kirill.han011@gmail.com>
2023-03-30 16:47:16 +0300

    добавлено поле координат и поле карты в модели. Созданы и применены миграции. реализовано добавление координат и карты в модели

--- zakaz/urls.py
+++ zakaz/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from zakaz.views import (view_order_pages, view_change_order_status, view_order_cadastral,
                          view_order,
-                         # download_igi_docx,
+                         download_igi_docx,
                          ajax_validate_cadastral_number)
 
 app_name = 'zakaz'
@@ -13,5 +13,5 @@ urlpatterns = [
     path('order/<slug:company_slug>/<slug:company_number_slug>/form', view_order, name='order'),
     path('order_pages/<slug:company_number_slug>/', view_order_pages, name='order_pages'),
     path('change_order_status/<int:order_id>/', view_change_order_status, name="change_order_status"),
-    # path('download_igi_docx/<int:pk>/', download_igi_docx, name='download_igi_docx'),
+    path('download_igi_docx/<int:pk>/', download_igi_docx, name='download_igi_docx'),
 ]
--- zakaz/views.py
+++ zakaz/views.py
@@ -17,7 +17,7 @@ from django.contrib import messages
 import folium
 import io
 import os
-# from docx import Document
+from docx import Document
 from django.http import HttpResponse
 from django.conf import settings
 
@@ -26,13 +26,12 @@ User = get_user_model()
 
 def ajax_validate_cadastral_number(request):
     cadastral_number = request.GET.get('cadastral_number', None)
-    a = ['24:39:0101001:369', '61:58:0002046:11']
     try:
         validate_number(cadastral_number)
         response = {
             'is_valid': True
         }
-        for number in a:
+        for number in cadastral_number:
             while True:
                 areas = GetArea(number)
                 coordinates = areas.get_coord()
@@ -146,7 +145,7 @@ def view_change_order_status(request, order_id):
     }
 
     return render(request, 'change_order_status.html', context=context)
-
+    
 
 def get_map(number_list):
     points = []
@@ -171,101 +170,101 @@ def get_map(number_list):
     map_html = m._repr_html_()
     return map_html
 
-# # Выгрузка DOCX
-# def replace_placeholders(paragraph, placeholders):
-#     for placeholder, value in placeholders.items():
-#         if placeholder in paragraph.text:
-#             for run in paragraph.runs:
-#                 if placeholder in run.text:
-#                     run.text = run.text.replace(placeholder, value)
-#
-#
-# # Замена слов по ключам в параграфах, заголовках и таблицах
-# def replace_placeholders_in_document(document, placeholders):
-#     for paragraph in document.paragraphs:
-#         replace_placeholders(paragraph, placeholders)
-#
-#     for table in document.tables:
-#         for row in table.rows:
-#             for cell in row.cells:
-#                 for paragraph in cell.paragraphs:
-#                     replace_placeholders(paragraph, placeholders)
-#
-#     return document
-#
-#
-# # Генерация нового документа
-# def generate_docx(document_path, placeholders):
-#     document = Document(document_path)
-#     replace_placeholders_in_document(document, placeholders)
-#     return document
-#
-#
-# # Скачивание DOCX
-# def download_docx(request, document_name, document_path, placeholders):
-#     document = generate_docx(document_path, placeholders)
-#
-#     output = io.BytesIO()
-#     document.save(output)
-#     output.seek(0)
-#
-#     response = HttpResponse(
-#         output,
-#         content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-#     )
-#     response['Content-Disposition'] = f'attachment; filename="{document_name}.docx"'
-#     return response
-#
-#
-# # Скачиваем ШИФР-ИГДИ
-# # def download_igdi_docx(request):
-# #     document_name = 'igdi'
-# #     document_path = os.path.join(settings.MEDIA_ROOT, f'{document_name}.docx')
-# #     placeholders = {
-# #         '_шифр-игди': 'Какой-то шифр ИГДИ',
-# #         '_должность_руководителя_ведомства': 'Директор',
-# #         '_название_ведомства': 'Ведомство всех ведомств',
-# #         '_фио_руководителя_ведомства': 'Иванов Иван Иванович',
-# #         '_тел_ведомства': '8 900 000 00 00',
-# #         '_почта_ведомства': 'vedomstvo@example.com',
-# #         '_дата_текущая': datetime.datetime.now().strftime("%Y-%m-%d"),
-# #         '_имя_руководителя_ведомства': 'Иван',
-# #         '_название_объекта_полное': 'Объект какой-то там',
-# #         '_кадастровый_номер': '47:23:0604008:451',
-# #         '_обзорная_схема': 'схема',
-# #         '_таблица_координат': 'координаты'
-# #     }
-# #     return download_docx(request, document_name, document_path, placeholders)
-#
-#
-# # Скачиваем ШИФР-ИГИ
-# def download_igi_docx(request, pk):
-#     order = get_object_or_404(Order, pk=pk)
-#     department = order.region.region_department.first()
-#     location = f"{order.region}, {order.area}, {order.city}, {order.street}, д.{order.house_number}"
-#     if order.building:
-#         location += f" {order.building}"
-#
-#     date = datetime.datetime.now()
-#
-#     document_name = 'igi'
+# Выгрузка DOCX
+def replace_placeholders(paragraph, placeholders):
+    for placeholder, value in placeholders.items():
+        if placeholder in paragraph.text:
+            for run in paragraph.runs:
+                if placeholder in run.text:
+                    run.text = run.text.replace(placeholder, value)
+
+
+# Замена слов по ключам в параграфах, заголовках и таблицах
+def replace_placeholders_in_document(document, placeholders):
+    for paragraph in document.paragraphs:
+        replace_placeholders(paragraph, placeholders)
+
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    replace_placeholders(paragraph, placeholders)
+
+    return document
+
+
+# Генерация нового документа
+def generate_docx(document_path, placeholders):
+    document = Document(document_path)
+    replace_placeholders_in_document(document, placeholders)
+    return document
+
+
+# Скачивание DOCX
+def download_docx(request, document_name, document_path, placeholders):
+    document = generate_docx(document_path, placeholders)
+
+    output = io.BytesIO()
+    document.save(output)
+    output.seek(0)
+
+    response = HttpResponse(
+        output,
+        content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    )
+    response['Content-Disposition'] = f'attachment; filename="{document_name}.docx"'
+    return response
+
+
+# Скачиваем ШИФР-ИГДИ
+# def download_igdi_docx(request):
+#     document_name = 'igdi'
 #     document_path = os.path.join(settings.MEDIA_ROOT, f'{document_name}.docx')
-#     if department:
-#         placeholders = {
-#             '_шифр-иги': f"{date.strftime('%Y%m%d')}-{order.pk:03d}",
-#             '_должность_руководителя_ведомства': department.director_position,
-#             '_название_ведомства': department.name,
-#             '_фио_руководителя_ведомства': f'{department.director_surname} {department.director_name} {department.director_patronymic}',
-#             '_тел_ведомства': f'{department.phone_number}',
-#             '_почта_ведомства': department.email,
-#             '_дата_текущая': date.strftime("%Y-%m-%d"),
-#             '_имя_руководителя_ведомства': department.director_name,
-#             '_название_объекта_полное': order.object_name,
-#             '_местоположение_объекта': location,
-#             '_кадастровый_номер': order.cadastral_number,
-#             '_обзорная_схема': 'схема',
-#             '_таблица_координат': 'координаты'
-#         }
-#     else:
-#         placeholders = {}
+#     placeholders = {
+#         '_шифр-игди': 'Какой-то шифр ИГДИ',
+#         '_должность_руководителя_ведомства': 'Директор',
+#         '_название_ведомства': 'Ведомство всех ведомств',
+#         '_фио_руководителя_ведомства': 'Иванов Иван Иванович',
+#         '_тел_ведомства': '8 900 000 00 00',
+#         '_почта_ведомства': 'vedomstvo@example.com',
+#         '_дата_текущая': datetime.datetime.now().strftime("%Y-%m-%d"),
+#         '_имя_руководителя_ведомства': 'Иван',
+#         '_название_объекта_полное': 'Объект какой-то там',
+#         '_кадастровый_номер': '47:23:0604008:451',
+#         '_обзорная_схема': 'схема',
+#         '_таблица_координат': 'координаты'
+#     }
 #     return download_docx(request, document_name, document_path, placeholders)
+
+
+# Скачиваем ШИФР-ИГИ
+def download_igi_docx(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    department = order.region.region_department.first()
+    location = f"{order.region}, {order.area}, {order.city}, {order.street}, д.{order.house_number}"
+    if order.building:
+        location += f" {order.building}"
+
+    date = datetime.datetime.now()
+
+    document_name = 'igi'
+    document_path = os.path.join(settings.MEDIA_ROOT, f'{document_name}.docx')
+    if department:
+        placeholders = {
+            '_шифр-иги': f"{date.strftime('%Y%m%d')}-{order.pk:03d}",
+            '_должность_руководителя_ведомства': department.director_position,
+            '_название_ведомства': department.name,
+            '_фио_руководителя_ведомства': f'{department.director_surname} {department.director_name} {department.director_patronymic}',
+            '_тел_ведомства': f'{department.phone_number}',
+            '_почта_ведомства': department.email,
+            '_дата_текущая': date.strftime("%Y-%m-%d"),
+            '_имя_руководителя_ведомства': department.director_name,
+            '_название_объекта_полное': order.object_name,
+            '_местоположение_объекта': location,
+            '_кадастровый_номер': order.cadastral_number,
+            '_обзорная_схема': 'схема',
+            '_таблица_координат': 'координаты'
+        }
+    else:
+        placeholders = {}
+    return download_docx(request, document_name, document_path, placeholders)
