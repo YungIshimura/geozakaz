7e1351a9417670ff170b80776acf6d66f8b8235d
axexeyvershinin <og2001@yandex.ru>
2023-04-03 14:49:08 +0300

    Changed the user registration and authorization system

--- users/models.py
+++ users/models.py
@@ -1,16 +1,49 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import Q
 from phonenumber_field.modelfields import PhoneNumberField
 from pytils.translit import slugify
+from phonenumber_field.phonenumber import to_python
 from django.utils.crypto import get_random_string
+import phonenumbers
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, phone_number, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set')
+        if not phone_number:
+            raise ValueError('Phone number must be set')
+        user = self.model(
+            email=self.normalize_email(email),
+            phone_number=phone_number,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, phone_number, password, **extra_fields)
+
+    def get_by_natural_key(self, username):
+        return self.get(Q(email=username) | Q(phone_number=username))
 
 
 class User(AbstractUser):
-    patronymic = models.CharField(
-        'Отчество',
-        max_length=30)
-    phone = PhoneNumberField(
-        'Телефон')
+    username = None
+    email = models.EmailField(
+        'Адрес электронной почты',
+        max_length=255,
+        unique=True)
+    phone_number = PhoneNumberField(
+        'Номер телефона',
+        unique=True,
+        null=True,
+        blank=True)
     agreement = models.BooleanField(
         'Принял условия соглашения',
         default=False)
@@ -35,6 +68,14 @@ class User(AbstractUser):
         null=True
     )
 
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone_number']
+
     def save(self, *args, **kwargs):
         self.company_slug = slugify(self.company_name)
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.email
