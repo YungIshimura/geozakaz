21990a5582b31244631f6a10f258f58b5346b60c
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-04-13 16:16:25 +0200

    Fix delete cadastrals

--- static/js/customer_home.js
+++ static/js/customer_home.js
@@ -2,6 +2,7 @@ let id = 1
 let flag = 1
 let array = []
 let dt = new DataTransfer();
+let all_files = 0 
 
 function GetSelectOption () {
     let button = document.getElementById('btn')
@@ -117,20 +118,20 @@ window.onload = function UploadFiles() {
 
     $fileInput.on('change', function() {
     let filesCount = $(this)[0].files.length;
-    let $textContainer = $(this).prev();
-    
 
+    
     if (filesCount) {
         document.getElementById('order-btn').disabled = false;
         document.getElementById('form-div').style.cssText = 'display:none';
         let div = document.getElementsByClassName('file-input-list')[0]
         let files = document.getElementById('file-input').files
-    
+        
         for(let i = 0; i < filesCount; i++){
-            div.innerHTML += `<div class="input-file-list-item" style="margin-top:5px;" id='custom${i}'>` +
+            div.innerHTML += `<div class="input-file-list-item" style="margin-top:5px;" id='custom${all_files}'>` +
                 '<span class="input-file-list-name"> <svg width="28" height="33" viewBox="0 0 28 33" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M17.8501 1L27.1176 9.72913M27.1176 9.72913H19.5351C19.0882 9.72913 18.6596 9.56192 18.3436 9.26428C18.0276 8.96663 17.8501 8.56295 17.8501 8.14202V1H2.68501C2.23812 1 1.80953 1.16721 1.49353 1.46486C1.17753 1.7625 1 2.16619 1 2.58711V30.3616C1 30.7826 1.17753 31.1862 1.49353 31.4839C1.80953 31.7815 2.23812 31.9487 2.68501 31.9487H25.4326C25.8795 31.9487 26.3081 31.7815 26.6241 31.4839C26.9401 31.1862 27.1176 30.7826 27.1176 30.3616V9.72913Z" stroke="#2D9CDB" stroke-linecap="round" stroke-linejoin="round"/> </svg>' + files[i].name + '</span>' +
-                `<a href="#" onclick="removeFilesItem(${i});" class="input-file-list-remove">        x</a>` +
-                '</div>'
+                `<a href="#" onclick="removeFilesItem(${all_files});" class="input-file-list-remove">        x</a>` +
+                '</div>'          
+            all_files += 1
         }
     }
     });
