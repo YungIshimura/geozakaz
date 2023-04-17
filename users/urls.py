fb3cc6ddf94217a1fa283309e29cdd1410d09359
alexey <og2001@yandex.ru>
2023-04-17 11:26:46 +0300

    Refactoring code

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 
-from .views import register_user, logout_account, view_agreement, login_company, \
-    login_user # ,view_index
+from .views import register_user, logout_account, view_agreement, login_company, login_user
 
 app_name = 'users'
 
 urlpatterns = [
-    # path('', view_index, name='index'),
-    path('login_user', login_user, name='user_login'),
     path('register/', register_user, name='user_register'),
+    path('login_user', login_user, name='user_login'),
     path('login_company', login_company, name='company_login'),
     path('logout/', logout_account, name='user_logout'),
-    path('agreement/', view_agreement, name='agreement'),
+    path('agreement/', view_agreement, name='agreement')
 ]
